056436ed298e9ad1665ca73364a4fa9e7fc98d52
bkalzhan <kalzhanbekzat99@gmail.com>
2024-10-23 14:29:17 -0400

    Task 1.2: changes home page banner

--- frontend/templates/HomePage/Banner/Banner.jsx
+++ frontend/templates/HomePage/Banner/Banner.jsx
@@ -3,57 +3,71 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
 
-import KaiBlob from '@/assets/images/Kai_blob.png';
-import rectangle from '@/assets/svg/Rectangle_Banner_1.svg';
+import Rectangle89 from '@/assets/svg/banner/Rectangle89.svg';
+import Rectangle90 from '@/assets/svg/banner/Rectangle90.svg';
+import Rectangle91 from '@/assets/svg/banner/Rectangle91.svg';
+import Rectangle92 from '@/assets/svg/banner/Rectangle92.svg';
+import Star7 from '@/assets/svg/banner/Star7.svg';
+import Star8 from '@/assets/svg/banner/Star8.svg';
+import Star2 from '@/assets/svg/Star_4.svg';
 import ImageURLs from '@/assets/urls';
 
 import styles from './styles';
 
 const Banner = () => {
   const renderLeftArtifacts = () => (
-    <Box position="relative" width="200px" height="200px" flexShrink={0}>
+    <Box
+      position="relative"
+      minWidth="169px"
+      minHeight="169px"
+      height="100%"
+      flexShrink={0}
+    >
+      <Box {...styles.star2Props}>
+        <Star2 />
+      </Box>
+      <Box {...styles.star7Props}>
+        <Star7 />
+      </Box>
       <Image
         src={ImageURLs.MarvelAuthImg}
         alt="Marvel"
         layout="fill"
-        objectFit="contain"
+        objectFit="cover"
       />
     </Box>
   );
 
   const renderTitle = () => (
-    <Box flex={1} mx={2}>
+    <Box flex={1} mx={2} maxWidth="48%">
       <Typography {...styles.titleProps}>
         Hello! Welcome to Kai Tools. 👋
       </Typography>
       <Typography {...styles.subtitleProps}>
         Made for{' '}
-        <Typography {...styles.highlightTextProps} component="span">
-          educators
-        </Typography>
-        . Hello! I&#39;m Kai, your AI teaching assistant. We are here to support
+        <Typography {...styles.highlightTextProps}>educators</Typography>.
+        Hello! I&#39;m Kai, your AI teaching assistant. We are here to support
         you on your journey as a
-        <Typography {...styles.boldTextProps} component="span">
-          {' '}
-          teacher, mentor,
-        </Typography>{' '}
-        and
-        <Typography {...styles.boldTextProps} component="span">
-          {' '}
-          more!
-        </Typography>
+        <Typography {...styles.boldTextProps}> teacher, mentor,</Typography> and
+        <Typography {...styles.boldTextProps}> more!</Typography>
       </Typography>
     </Box>
   );
 
   const renderRightArtifacts = () => (
-    <Box position="relative" width="100px" height="100px" flexShrink={0}>
-      <Image
-        src={ImageURLs.MarvelAuthImg}
-        alt="Decorative"
-        layout="fill"
-        objectFit="contain"
-      />
+    <Box position="relative" width="265px" height="169px" flexShrink={0}>
+      <Box {...styles.rectangle89Props}>
+        <Rectangle89 />
+      </Box>
+      <Box {...styles.rectangle90Props}>
+        <Rectangle90 />
+      </Box>
+      <Box {...styles.rectangle91Props}>
+        <Rectangle91 />
+      </Box>
+      <Box {...styles.rectangle92Props}>
+        <Rectangle92 />
+      </Box>
     </Box>
   );
 
@@ -63,12 +77,16 @@ const Banner = () => {
       display="flex"
       flexDirection="row"
       alignItems="center"
+      overflow="hidden"
+      height="169px"
       justifyContent="space-between"
-      p={2}
     >
       {renderLeftArtifacts()}
       {renderTitle()}
       {renderRightArtifacts()}
+      <Box {...styles.star8Props}>
+        <Star8 />
+      </Box>
     </Box>
   );
 };
--- frontend/templates/HomePage/Banner/styles.js
+++ frontend/templates/HomePage/Banner/styles.js
@@ -8,27 +8,81 @@ const styles = {
   },
   titleProps: {
     fontFamily: 'Satoshi Bold',
-    fontSize: '36px',
+    fontSize: '30px',
     color: (theme) => theme.palette.Text.Primary,
   },
   subtitleProps: {
     fontFamily: 'Satoshi Regular',
-    fontSize: '18px',
+    fontSize: '15px',
     color: (theme) => theme.palette.Text.Primary,
+    maxWidth: '90%',
   },
   boldTextProps: {
-    component: 'span',
     fontFamily: 'inherit',
-    fontSize: 'inherit',
-    fontWeight: 'bold',
-    color: (theme) => theme.palette.Text.Primary,
+    component: 'span',
+    fontWeight: '700',
   },
   highlightTextProps: {
-    component: 'span',
     fontFamily: 'inherit',
-    fontSize: 'inherit',
-    fontWeight: 'bold',
+    component: 'span',
+    fontWeight: '700',
     color: (theme) => theme.palette.primary.main,
+  },
+  star2Props: {
+    sx: (theme) => ({
+      position: 'absolute',
+      left: theme.spacing(11),
+      top: theme.spacing(13),
+      width: theme.spacing(5),
+      zIndex: '1',
+    }),
+  },
+  star7Props: {
+    sx: (theme) => ({
+      position: 'absolute',
+      left: theme.spacing(18),
+      top: theme.spacing(2),
+    }),
+  },
+  star8Props: {
+    sx: (theme) => ({
+      position: 'relative',
+      right: theme.spacing(1),
+      bottom: theme.spacing(-8.5),
+    }),
+  },
+  rectangle89Props: {
+    sx: (theme) => ({
+      position: 'absolute',
+      left: theme.spacing(0),
+      bottom: theme.spacing(-1),
+      width: {
+        laptop: 20,
+        desktop: 20,
+        desktopMedium: 20,
+      },
+    }),
+  },
+  rectangle90Props: {
+    sx: (theme) => ({
+      position: 'absolute',
+      left: theme.spacing(15.5),
+      top: theme.spacing(0),
+    }),
+  },
+  rectangle91Props: {
+    sx: (theme) => ({
+      position: 'absolute',
+      left: theme.spacing(25.3),
+      top: theme.spacing(0),
+    }),
+  },
+  rectangle92Props: {
+    sx: (theme) => ({
+      position: 'absolute',
+      left: theme.spacing(13),
+      top: theme.spacing(0),
+    }),
   },
 };
 
